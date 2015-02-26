61414c9a3eeaf8f5c0b6b94a5fa28f17d47cd34b
Laurin Brandner <mail@laurinbrandner.ch>
2015-02-26 14:01:40 +0100

    Add some files

---
+++ BRNImagePickerSheet/BRNImagePickerSheet/BRNImagePickerSheet.h
@@ -0,0 +1,19 @@
+//
+//  BRNImagePickerSheet.h
+//  BRNImagePickerSheet
+//
+//  Created by Laurin Brandner on 26/02/15.
+//  Copyright (c) 2015 Laurin Brandner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BRNImagePickerSheet.
+FOUNDATION_EXPORT double BRNImagePickerSheetVersionNumber;
+
+//! Project version string for BRNImagePickerSheet.
+FOUNDATION_EXPORT const unsigned char BRNImagePickerSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BRNImagePickerSheet/PublicHeader.h>
+
+#import "BRNImagePickerSheet.h"
