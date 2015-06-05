397329098c4acdb2348791c916b444103423e7bd
Laurin Brandner <mail@laurinbrandner.ch>
2015-06-05 17:16:55 +0200

    Add KIF extensions

---
+++ ImagePickerSheetController/ImagePickerSheetControllerUITests/KIFExtensions.swift
@@ -0,0 +1,30 @@
+//
+//  KIFExtensions.swift
+//  ImagePickerSheetController
+//
+//  Created by Laurin Brandner on 05/06/15.
+//  Copyright (c) 2015 Laurin Brandner. All rights reserved.
+//
+
+import Foundation
+import KIF
+
+extension XCTestCase {
+    func tester(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+    
+    func system(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
+
+extension KIFTestActor {
+    func tester(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+    
+    func system(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
