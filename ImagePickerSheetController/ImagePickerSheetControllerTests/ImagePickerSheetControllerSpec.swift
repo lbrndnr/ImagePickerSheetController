2df34e2ae653eb0b7bd9b162cbe2ac8fc94b96c1
Laurin Brandner <mail@laurinbrandner.ch>
2015-06-05 18:16:59 +0200

    Use only one test target

--- ImagePickerSheetController/ImagePickerSheetControllerTests/ImagePickerSheetControllerSpec.swift
+++ ImagePickerSheetController/ImagePickerSheetControllerTests/ImagePickerSheetControllerSpec.swift
@@ -1,6 +1,6 @@
 //
-//  ImagePickerSheetControllerTests.swift
-//  ImagePickerSheetControllerTests
+//  ImagePickerSheetControllerSpec.swift
+//  ImagePickerSheetControllerSpec
 //
 //  Created by Laurin Brandner on 26/05/15.
 //  Copyright (c) 2015 Laurin Brandner. All rights reserved.
@@ -8,6 +8,7 @@
 
 import UIKit
 import XCTest
+import KIF
 import Quick
 import Nimble
 import ImagePickerSheetController
@@ -15,17 +16,31 @@ import ImagePickerSheetController
 class ImagePickerSheetControllerSpec: QuickSpec {
     
     override func spec() {
-        var controller: ImagePickerSheetController!
+        let rootViewController = (UIApplication.sharedApplication().windows.first as! UIWindow).rootViewController!
+        var imageController: ImagePickerSheetController!
         
         beforeEach {
-            controller = ImagePickerSheetController()
+            imageController = ImagePickerSheetController()
         }
         
-        it("should not add two cancel actions") {
-            controller.addAction(ImageAction(title: "Cancel1", style: .Cancel))
-            expect {
-                controller.addAction(ImageAction(title: "Cancel2", style: .Cancel))
-            }.to(raiseException())
+        describe("presentation") {
+            
+            it("should present") {
+                rootViewController.presentViewController(imageController, animated: true, completion: nil)
+                let sheet = self.tester().waitForViewWithAccessibilityIdentifier("controller")
+                
+                expect(sheet).notTo(beNil())
+            }
+            
+        }
+        
+        describe("actions") {
+            it("should not add two cancel actions") {
+                imageController.addAction(ImageAction(title: "Cancel1", style: .Cancel))
+                expect {
+                    imageController.addAction(ImageAction(title: "Cancel2", style: .Cancel))
+                }.to(raiseException())
+            }
         }
     }
     
