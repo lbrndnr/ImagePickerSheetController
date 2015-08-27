d26e58ad474eaf309138d1d886df4ba8c83f7daf
Laurin Brandner <mail@laurinbrandner.ch>
2015-08-27 22:09:02 +0200

    Improve accessibility

--- ImagePickerSheetController/ImagePickerSheetController/Sheet/SheetActionCollectionViewCell.swift
+++ ImagePickerSheetController/ImagePickerSheetController/Sheet/SheetActionCollectionViewCell.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class SheetActionCollectionViewCell: SheetCollectionViewCell {
+let KVOContext = UnsafeMutablePointer<()>()
 
+class SheetActionCollectionViewCell: SheetCollectionViewCell {
+    
     lazy private(set) var textLabel: UILabel = {
         let label = UILabel()
         label.textColor = self.tintColor
@@ -19,6 +21,39 @@ class SheetActionCollectionViewCell: SheetCollectionViewCell {
         
         return label
     }()
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    private func initialize() {
+        textLabel.addObserver(self, forKeyPath: "text", options: NSKeyValueObservingOptions(rawValue: 0), context: KVOContext)
+    }
+    
+    deinit {
+        textLabel.removeObserver(self, forKeyPath: "text")
+    }
+    
+    // MARK: - Accessibility
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        guard context == KVOContext else {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+            return
+        }
+        
+        accessibilityLabel = textLabel.text
+    }
+    
+    // MARK: -
     
     override func tintColorDidChange() {
         super.tintColorDidChange()
