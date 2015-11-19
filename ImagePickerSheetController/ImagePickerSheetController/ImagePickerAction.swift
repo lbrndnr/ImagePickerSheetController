3359bb9bf915aef4cff6976160ef6d3e05508302
Rob Hudson <rhudson@weddingwire.com>
2015-11-19 15:39:41 -0500

    Add reset option to actions

--- ImagePickerSheetController/ImagePickerSheetController/ImagePickerAction.swift
+++ ImagePickerSheetController/ImagePickerSheetController/ImagePickerAction.swift
@@ -28,20 +28,23 @@ public class ImagePickerAction {
     /// The style of the action. This is used to call a cancel handler when dismissing the controller by tapping the background.
     public let style: ImagePickerActionStyle
     
+    /// Set to 'true' to reset the currently selected images when the action is selected.
+    public let reset: Bool
+    
     let handler: Handler
     let secondaryHandler: SecondaryHandler
     
     /// Initializes a new ImagePickerAction. The secondary title and handler are used when at least 1 image has been selected.
     /// Secondary title defaults to title if not specified.
     /// Secondary handler defaults to handler if not specified.
-    public convenience init(title: String, secondaryTitle: String? = nil, style: ImagePickerActionStyle = .Default, handler: Handler, secondaryHandler: SecondaryHandler? = nil) {
-        self.init(title: title, secondaryTitle: secondaryTitle.map { string in { _ in string }}, style: style, handler: handler, secondaryHandler: secondaryHandler)
+    public convenience init(title: String, secondaryTitle: String? = nil, style: ImagePickerActionStyle = .Default, reset: Bool = false, handler: Handler, secondaryHandler: SecondaryHandler? = nil) {
+        self.init(title: title, secondaryTitle: secondaryTitle.map { string in { _ in string }}, style: style, reset: reset, handler: handler, secondaryHandler: secondaryHandler)
     }
     
     /// Initializes a new ImagePickerAction. The secondary title and handler are used when at least 1 image has been selected.
     /// Secondary title defaults to title if not specified. Use the closure to format a title according to the selection.
     /// Secondary handler defaults to handler if not specified
-    public init(title: String, secondaryTitle: Title?, style: ImagePickerActionStyle = .Default, handler: Handler, var secondaryHandler: SecondaryHandler? = nil) {
+    public init(title: String, secondaryTitle: Title?, style: ImagePickerActionStyle = .Default, reset: Bool = false, handler: Handler, var secondaryHandler: SecondaryHandler? = nil) {
         if secondaryHandler == nil {
             secondaryHandler = { action, _ in
                 handler(action)
@@ -51,6 +54,7 @@ public class ImagePickerAction {
         self.title = title
         self.secondaryTitle = secondaryTitle ?? { _ in title }
         self.style = style
+        self.reset = reset
         self.handler = handler
         self.secondaryHandler = secondaryHandler!
     }
