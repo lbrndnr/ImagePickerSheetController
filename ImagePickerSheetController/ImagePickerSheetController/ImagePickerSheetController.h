c96c356afa4589ebec026f4063bf445a1a27a158
Laurin Brandner <mail@laurinbrandner.ch>
2016-10-14 11:48:36 +0200

    Remove unnecessary lines

--- ImagePickerSheetController/ImagePickerSheetController/ImagePickerSheetController.h
+++ ImagePickerSheetController/ImagePickerSheetController/ImagePickerSheetController.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double ImagePickerSheetControllerVersionNumber;
 
 //! Project version string for ImagePickerSheetController.
 FOUNDATION_EXPORT const unsigned char ImagePickerSheetControllerVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ImagePickerSheetController/PublicHeader.h>
-
-
