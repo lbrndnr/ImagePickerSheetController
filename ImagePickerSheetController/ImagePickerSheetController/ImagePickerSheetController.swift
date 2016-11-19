7afc7b0a2e6a18a8540656c8efbc6498d3e6df31
Patrick Balestra <me@patrickbalestra.com>
2016-11-19 16:52:57 +0100

    Fix crash when setting maximum selection to 0 and deselecting an image

--- ImagePickerSheetController/ImagePickerSheetController/ImagePickerSheetController.swift
+++ ImagePickerSheetController/ImagePickerSheetController/ImagePickerSheetController.swift
@@ -426,16 +426,16 @@ extension ImagePickerSheetController: UICollectionViewDataSource {
 extension ImagePickerSheetController: UICollectionViewDelegate {
     
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let maximumSelection = maximumSelection {
-            if selectedAssetIndices.count >= maximumSelection,
-                let previousItemIndex = selectedAssetIndices.first {
-                    let deselectedAsset = selectedAssets[previousItemIndex]
-                    delegate?.controller?(self, willDeselectAsset: deselectedAsset)
-                
-                    supplementaryViews[previousItemIndex]?.selected = false
-                    selectedAssetIndices.remove(at: 0)
-                
-                    delegate?.controller?(self, didDeselectAsset: deselectedAsset)
+        if let maximumSelection = maximumSelection, selectedAssetIndices.count >= maximumSelection {
+            if let previousItemIndex = selectedAssetIndices.first {
+                guard let deselectedAsset = selectedAssets.first else { return }
+
+                delegate?.controller?(self, willDeselectAsset: deselectedAsset)
+
+                supplementaryViews[previousItemIndex]?.selected = false
+                selectedAssetIndices.removeFirst()
+
+                delegate?.controller?(self, didDeselectAsset: deselectedAsset)
             }
         }
         
